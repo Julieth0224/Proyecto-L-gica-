6043db59806561829f0572e843e55ae94f47ae33
Oscarvch03 <oscar.velasco@urosario.edu.co>
2019-11-08 10:36:35 -0500

    Tseitin y DPLL

---
+++ codigo_main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 09:21:35 2019
+@author: hipatia
+"""
+
+# -*- coding: utf-8 -*-
+
+# Transformación de una formula en forma clausal
+# Input: cadena de la formula en notacion inorder
+# Output: lista de listas de literales
+
+# Importando la libreria FNC
+import FNC as fn
+letrasProposicionalesA = ['p', 'q', 'r', 's', 't']
+# # Formula a la cual encontrar su forma clausal
+## Holi cambie algo x2
+formula = "(pYq)"
+
+# Aplicando el algoritmo de Tseitin a formula
+# Se obtiene una cada que representa la formula en FNC
+fFNC = fn.Tseitin(formula, letrasProposicionalesA)
+print(fFNC)
+
+# Se obtiene la forma clausal como lista de listas de literales
+fClaus = fn.formaClausal(fFNC)
+
+# Imprime el resultado en consola
+print(fClaus)
